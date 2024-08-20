35ea3305081b585d195f99b8c16e22684655b029
Loreeh_ <128195197+Loreehh@users.noreply.github.com>
2024-08-20 13:01:33 +0200

    Added search bar
    Added search bar + function with table content

--- scripts.js
+++ scripts.js
@@ -20,3 +20,22 @@ function toggleTheme() {
       document.getElementById('slider').checked = true;
     }
 })();
+
+function searchTable() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const table = document.getElementById('cointable');
+    const tr = table.getElementsByTagName('tr');
+
+    for (let i = 1; i < tr.length; i++) {
+        const td = tr[i].getElementsByTagName('td')[0]; 
+        if (td) {
+            const txtValue = td.textContent || td.innerText;
+            if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                tr[i].style.display = '';
+            } else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
